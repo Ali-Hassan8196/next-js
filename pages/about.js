48b54523955ad6a3a14370669f8b13774512c3d8
unknown <amnaabid306@gmail.com>
2023-04-03 15:31:57 +0500

    updated

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <Link href="/">home</Link>
+    </div>
+  );
+}
+export default About;
---
+++ pages/api/index.js
@@ -0,0 +1,3 @@
+export default function handler(req, res) {
+  res.status(200).json({ name: "My First Api in Next Js" });
+}
